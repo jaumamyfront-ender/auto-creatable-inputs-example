61f45216037cce8acd95ab231d98d207f2fd63b4
yaumamyfront-ender <nepobedimtjpobedit@gmail.com>
2025-04-23 22:00:19 +0200

    exp

--- components/userPage.tsx
+++ components/userPage.tsx
@@ -1,7 +1,11 @@
 // import { CreatorDetailsWithVideosDTO } from "@/api-models/creatorDetailsWithVideosDTO";
 // import { VideoRecommendationDTO } from "@/api-models/videoRecommendationDTO";
+import { CreatorDetailsWithVideosDTO } from "@/api-models/creatorDetailsWithVideosDTO";
+import { CreatorProductRecomendationDTO } from "@/api-models/creatorProductRecomendationDTO";
+import { VideoRecommendationDTO } from "@/api-models/videoRecommendationDTO";
 import { UserDetails } from "@/types/User";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function ({ data }: UserDetails) {
   console.log(data);
@@ -25,7 +29,7 @@ export default function ({ data }: UserDetails) {
         </div>
       </header>
       <h2 className="text-black text-lg">my films</h2>
-      <section
+      {/* <section
         id="productsList"
         className="flex flex-row flex-wrap justify-center "
       >
@@ -63,9 +67,10 @@ export default function ({ data }: UserDetails) {
             </div>
           );
         })}
-      </section>
+      </section> */}
       <h2 className="text-black">my videos</h2>
-      <section
+      <Section data={data} isVideoSection={false} />
+      {/* <section
         id="productsList"
         className="flex flex-row flex-wrap justify-center "
       >
@@ -98,7 +103,76 @@ export default function ({ data }: UserDetails) {
             </div>
           );
         })}
-      </section>
+      </section> */}
+
+      <Section data={data} isVideoSection={true} />
     </div>
   );
 }
+interface SectionProps {
+  data: CreatorDetailsWithVideosDTO;
+  isVideoSection: boolean;
+}
+const Section = ({ data, isVideoSection }: SectionProps) => {
+  const slicedItems = isVideoSection
+    ? data.videos.slice(0, 3)
+    : data.products.slice(0, 3);
+
+  return (
+    <div>
+      <section
+        id="productsList"
+        className="flex flex-row flex-wrap justify-center "
+      >
+        {slicedItems.map((item, index) => {
+          const thumbnail = item.thumbnails?.find(
+            (img) => img.width === "pX512"
+          );
+
+          let description = "";
+          let priceText = "";
+
+          if (!isVideoSection) {
+            const productItem = item as CreatorProductRecomendationDTO;
+            const price = productItem?.price.find(
+              (price) => price.currency === "pln"
+            );
+            description = productItem.productName;
+            priceText = price ? `${price.price} PLN` : "Brak ceny";
+          } else {
+            const videoItem = item as VideoRecommendationDTO;
+            description = videoItem.videoTitle;
+            priceText = "";
+          }
+
+          return (
+            <div
+              className="flex items-center flex-col border border-amber-500 p-2 m-4 rounded-lg w-auto h-auto justify-between"
+              key={index}
+            >
+              <div className="flex flex-row items-center mb-12 justify-between">
+                <p className="text-black h-[48px] max-w-[200px]">
+                  {description}
+                </p>
+                {isVideoSection && (
+                  <p className="text-black flex self-start ml-2">{priceText}</p>
+                )}
+              </div>
+
+              {thumbnail && (
+                <Image
+                  key={index}
+                  className="border rounded-lg h-28"
+                  src={thumbnail.photoUrl}
+                  width={180}
+                  height={180}
+                  alt="Product thumbnail"
+                />
+              )}
+            </div>
+          );
+        })}
+      </section>
+    </div>
+  );
+};
